c11b0ca559ac17a266e7743468f674cf7b164e8a
Sagar Dhadke <peaceunique2003@gmail.com>
2023-06-25 14:41:46 +0530

    Added Binding and App Logo

--- app/src/main/java/net/uniquecomputer/lockqr/MainActivity.kt
+++ app/src/main/java/net/uniquecomputer/lockqr/MainActivity.kt
@@ -2,10 +2,15 @@ package net.uniquecomputer.lockqr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.uniquecomputer.lockqr.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
