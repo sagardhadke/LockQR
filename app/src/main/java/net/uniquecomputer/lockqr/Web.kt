2952f5841b76e0cda524138a4072d4695c563c8d
Sagar Dhadke <peaceunique2003@gmail.com>
2023-07-05 14:23:50 +0530

    Added Http and https protocol in web.kt

--- app/src/main/java/net/uniquecomputer/lockqr/Web.kt
+++ app/src/main/java/net/uniquecomputer/lockqr/Web.kt
@@ -20,49 +20,34 @@ class Web : AppCompatActivity() {
         binding = ActivityWebBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //set values to the spinner
-        val adapter = ArrayAdapter.createFromResource(this, R.array.spinnerItems, android.R.layout.simple_spinner_item)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spinner.adapter = adapter
+        val protocol = resources.getStringArray(R.array.protocol)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown, protocol)
+        binding.autoprotocol.setAdapter(arrayAdapter)
 
-        binding.spinner.setSelection(0)
-
-        binding.checkwebid.setOnClickListener {
-            MotionToast.createToast(this,
-                "Failed ☹️",
-                "Please Select a Valid Option!",
-                MotionToastStyle.ERROR,
-                MotionToast.GRAVITY_BOTTOM,
-                MotionToast.LONG_DURATION,
-                ResourcesCompat.getFont(this,R.font.poppins_semibold))
-            validate()
-        }
+//        binding.checkwebid.setOnClickListener {
+//            MotionToast.createToast(this,
+//                "Failed ☹️",
+//                "Please Select a Valid Option!",
+//                MotionToastStyle.ERROR,
+//                MotionToast.GRAVITY_BOTTOM,
+//                MotionToast.LONG_DURATION,
+//                ResourcesCompat.getFont(this,R.font.poppins_semibold))
+//            validate()
+//        }
 
     }
 
-    private fun validate() {
-
-        if (binding.spinner.selectedItemPosition == 0){
-            Toast.makeText(this, "Please Select a Valid Option", Toast.LENGTH_SHORT).show()
-            binding.spinner.requestFocus()
-        }else{
-            binding.spinner.selectedItemPosition == 1
-            binding.spinner.setBackgroundColor(resources.getColor(R.color.http))
-
-        }
-        if (binding.spinner.selectedItemPosition == 2){
-            binding.spinner.setBackgroundColor(resources.getColor(R.color.https))
-
-        }
-
-        if (binding.editText.text.isEmpty()){
-            binding.editText.error = "Please Enter a Valid Web Address"
-            binding.editText.requestFocus()
-        }else{
-            binding.editText.clearFocus()
-            binding.editText.isCursorVisible = false
-        }
-    }
+//    private fun validate() {
+//
+//
+//        if (binding.editText.text.isEmpty()){
+//            binding.editText.error = "Please Enter a Valid Web Address"
+//            binding.editText.requestFocus()
+//        }else{
+//            binding.editText.clearFocus()
+//            binding.editText.isCursorVisible = false
+//        }
+//    }
 
 
 }
