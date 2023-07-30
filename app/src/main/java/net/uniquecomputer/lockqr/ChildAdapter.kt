633b897d0af01cbf967a570ce0f45cbbcfb40599
Sagar Dhadke <peaceunique2003@gmail.com>
2023-07-30 19:28:22 +0530

    Update and Optimize code

--- app/src/main/java/net/uniquecomputer/lockqr/ChildAdapter.kt
+++ app/src/main/java/net/uniquecomputer/lockqr/ChildAdapter.kt
@@ -7,20 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import net.uniquecomputer.lockqr.Utils.Constants
 import net.uniquecomputer.lockqr.databinding.ActivityQrcodeBinding
 import net.uniquecomputer.lockqr.databinding.ChildIteamBinding
 
-class ChildAdapter(private val context: Context, private val childItem: List<ChildItem>,private val activityQrcodeBinding: ActivityQrcodeBinding? = null) :
+class ChildAdapter(private val context: Context, private val childItem: List<ChildItem>, private val activityQrcodeBinding: ActivityQrcodeBinding? = null) :
     RecyclerView.Adapter<ChildAdapter.ChildViewHolder>() {
 
-    inner class ChildViewHolder(private val binding: ChildIteamBinding): RecyclerView.ViewHolder(binding.root){
+    inner class ChildViewHolder(private val binding: ChildIteamBinding) : RecyclerView.ViewHolder(binding.root) {
         val title = binding.childIteamText
         val image = binding.childIteamImage
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChildViewHolder {
-       val view = ChildIteamBinding.inflate(LayoutInflater.from(context),parent,false)
+        val view = ChildIteamBinding.inflate(LayoutInflater.from(context), parent, false)
         return ChildViewHolder(view)
     }
 
@@ -29,34 +28,18 @@ class ChildAdapter(private val context: Context, private val childItem: List<Chi
     }
 
     override fun onBindViewHolder(holder: ChildViewHolder, position: Int) {
-        holder.image.setImageResource(childItem[position].image)
-        holder.title.text = childItem[position].title
+        val item = childItem[position]
+        holder.image.setImageResource(item.image)
+        holder.title.text = item.title
 
         holder.itemView.setOnClickListener {
             val intent = Intent(context, QRCode::class.java)
             context.startActivity(intent)
 
-            when(position) {
-                0 -> {
-                   activityQrcodeBinding?.name?.visibility = View.VISIBLE
-                }
-
-                1 -> {
-                    Toast.makeText(
-                        context,
-                        "Clicked on ${childItem[position].title}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-
-                2 -> {
-                    Toast.makeText(
-                        context,
-                        "Clicked on ${childItem[position].title}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-
+            when (position) {
+                0 -> activityQrcodeBinding?.name?.visibility = View.VISIBLE
+                1 -> Toast.makeText(context, "Clicked on ${item.title}", Toast.LENGTH_SHORT).show()
+                2 -> Toast.makeText(context, "Clicked on ${item.title}", Toast.LENGTH_SHORT).show()
             }
         }
     }
